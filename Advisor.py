7dc61f1ab90f5c7b17857093933317481c8a8be7
Pavel Fomin <webdev.fominpa@gmail.com>
2024-10-23 21:55:24 +0300

    Advisor:: take out OpenMethod to new class

--- Advisor.py
+++ Advisor.py
@@ -3,6 +3,68 @@ from sets import botdata
 import asyncio, aiohttp
 import buttons
 from UsersDB import UsersDB
+from urllib.parse import quote
+
+class OpenAdvice:
+	""" Command :: create or reopen advice & set appstate 'advisor' """
+	async def init(self, UserID):
+		await self.set_advisor_state(UserID) # DB
+		TopicID = await self.get_topic_id(UserID) # DB
+		if TopicID:
+			await self.reopen_advice(UserID) # TG
+		else:
+			TopicID = await self.create_topic(UserID) # TG
+			await self.write_new_topic(UserID, TopicID) # DB
+		await self.end_advice_button(UserID) # TG
+
+	async def set_advisor_state(self, UserID):
+		sql = 'UPDATE `BotUsers` SET appstate = \'advisor\' WHERE user_id=' + str(UserID) + ';'
+		await aconexe(sql)
+
+	# Создать топик с обращением
+	async def create_topic(self, UserID) -> 'TopicID: chat_id':
+		result = None
+		async with aiohttp.ClientSession() as session:
+			async with session.get(botdata.BASE_URL + 
+			'createForumTopic?chat_id=' + botdata.FORUM_ID +
+			'&name=Обращение от пользователя: ' + str(UserID)) as response:
+				result = await response.json()
+		if result['ok']:
+			return result['result']['message_thread_id']
+
+	# Записать топик в БД
+	async def write_new_topic(self, UserID, TopicID):
+		sql = 'UPDATE `BotUsers` set topic=' + str(TopicID) + ' WHERE user_id=' + str(UserID) + ';'
+		await aconexe(sql)
+
+	# Async скрипт нового обращения
+	async def new_advice_group(self, UserID):
+		TopicID = await self.create_topic(UserID)
+		await self.write_new_topic(UserID, TopicID)
+
+	async def get_topic_id(self, UserID) -> 'is have ? TopicID : None':
+		sql = 'SELECT topic FROM BotUsers WHERE user_id=' + str(UserID) + ';'
+		fetchall = await aconexe(sql)
+		if fetchall: 
+			return fetchall[0][0]
+
+	async def end_advice_button(self, UserID):
+		header = quote('Создано новое обращение:\n\n' +
+		'Задайте ваш вопрос, и наши специалисты на него ответят или нажмите \"Закончить диалог\", чтобы вернуться к меню')
+		header += '&parse_mode=html'
+		Markup = {
+			'keyboard': [
+				[{'text': 'Закончить диалог'},]
+			],
+			'resize_keyboard': True,
+			'one_time_keyboard': True
+		}
+		await buttons.send_markup(UserID, Markup, header)
+
+	async def reopen_advice(self, TopicID):
+		async with aiohttp.ClientSession() as session:
+			await session.get(botdata.BASE_URL + 'reopenForumTopic?chat_id=' + 
+			botdata.FORUM_ID + '&message_thread_id=' + str(TopicID))
 
 class Advisor:
 	async def init(self, Query):
@@ -24,18 +86,12 @@ class Advisor:
 				if fetchall:
 					if fetchall[0][1] == 'advisor':
 						return None
-				await self.set_advisor_state(UserID)
-				await self.new_advice_group(UserID)
-				await self.end_advice_button(UserID)
+				await OpenAdvice().init(UserID)
 				return
 			elif Query['callback_query']['data'] == 'closeadvise':
 				await self.set_default_state(UserID)
 				await self.close_topic(UserID)
 				return
-
-	async def set_advisor_state(self, UserID):
-		sql = 'UPDATE `BotUsers` SET appstate = \'advisor\' WHERE user_id=' + str(UserID) + ';'
-		await aconexe(sql)
 
 	async def set_default_state(self, UserID):
 		sql = 'UPDATE `BotUsers` SET appstate = Default WHERE user_id=' + str(UserID) + ';'
@@ -44,30 +100,8 @@ class Advisor:
 	async def close_topic(self, UserID):
 		TopicID = await self.get_topic_id(UserID)
 		async with aiohttp.ClientSession() as session:
-			await session.get(botdata.BASE_URL + 'deleteforumtopic?' +
+			await session.get(botdata.BASE_URL + 'closeforumtopic?' +
 				'chat_id=' + botdata.FORUM_ID + '&message_thread_id=' + str(TopicID))
-		sql = 'UPDATE `BotUsers` SET topic = Default WHERE user_id=' + str(UserID) + ';'
-		await aconexe(sql)
-
-	# Создать топик с обращением
-	async def create_topic(self, UserID) -> 'TopicID: chat_id':
-		result = None
-		async with aiohttp.ClientSession() as session:
-			async with session.get(botdata.BASE_URL + 
-			'createForumTopic?chat_id=' + botdata.FORUM_ID +
-			'&name=Обращение от пользователя: ' + str(UserID)) as response:
-				result = await response.json()
-		if result['ok']:
-			return result['result']['message_thread_id']
-	# Записать топик в БД
-
-	async def write_new_topic(self, UserID, TopicID):
-		sql = 'UPDATE `BotUsers` set topic=' + str(TopicID) + ' WHERE user_id=' + str(UserID) + ';'
-		await aconexe(sql)
-	# Async скрипт нового обращения
-	async def new_advice_group(self, UserID):
-		TopicID = await self.create_topic(UserID)
-		await self.write_new_topic(UserID, TopicID)
 
 	async def get_user_id(self, TopicID) -> 'is have ? UserID : None':
 		sql = 'SELECT user_id FROM BotUsers WHERE topic=' + str(TopicID) + ';'
@@ -85,7 +119,8 @@ class Advisor:
 	async def get_topic_id(self, UserID) -> 'is have ? TopicID : None':
 		sql = 'SELECT topic FROM BotUsers WHERE user_id=' + str(UserID) + ';'
 		fetchall = await aconexe(sql)
-		if fetchall: return fetchall[0][0]
+		if fetchall: 
+			return fetchall[0][0]
 
 	async def question_to_topic(self, Query):
 		TopicID = await self.get_topic_id(Query['message']['from']['id'])
@@ -95,12 +130,3 @@ class Advisor:
 				'&message_thread_id=' + str(TopicID) +
 				'&from_chat_id=' + str(Query['message']['from']['id']) +
 				'&message_id=' + str(Query['message']['message_id']))
-
-	async def end_advice_button(self, UserID):
-		Markup = {
-			'keyboard': [
-				[{'text': 'Закончить диалог'},]
-			],
-			'resize_keyboard': True
-		}
-		await buttons.send_markup(UserID, Markup, 'Чтобы закончить обращение, нажмите кнопку на клавиатуре телеграмма')
