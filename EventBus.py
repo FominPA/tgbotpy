45fdf5c36cb7fc304b2f3f29ef21e69f99b083f5
Pavel Fomin <webdev.fominpa@gmail.com>
2024-10-28 21:07:28 +0300

    Добавил некоторые проверки в general handler, немного разбил на состояния запроса на будущее

--- EventBus.py
+++ EventBus.py
@@ -24,51 +24,59 @@ class EventBus:
 	###############################################################################
 
 	async def init (self, Query: 'json'):
-
+		# Если это сообщение и из форума:
 		if 'message' in Query:
-			if Query['message']['chat']['type'] == 'supergroup':
+			if 'group' in Query['message']['chat']['type']: # group or supergroup
 				if 'is_topic_message' in Query['message']:
 					await Advisor().init(Query)
 				return
 
+		# Если это сообщение от пользователя в личке бота:
 		UserID = self.get_user_id(Query)
-
 		if 'message' in Query:
-			if Query['message']['chat']['type'] == 'private':
-				is_have_user_db = await UsersDB().init(UserID)
+			if 'private' in Query['message']['chat']['type']:
+
+				###################################################################
+				###################### private message state ######################
+
+				UserData = UsersDB()
+				await UserData.init()
+
 				from MessageCounter import MessageCounter
-				if is_have_user_db:
-					if not is_have_user_db[0][3]: # return message_counter
-						print(is_have_user_db[0][3])
-						await MessageCounter().set_first(UserID, Query['message']['message_id'])
-				else:
+				if not UserData.message_counter_setted(): 
 					await MessageCounter().set_first(UserID, Query['message']['message_id'])
 
-				if 'message' in Query:
-					if 'text' in Query['message']:
-						if Query['message']['text'] == '/start':
-							from menu.menu import Menu
-							await Menu().init(Query['message']['from']['id'])
-							return
-						if Query['message']['text'] == '/clear':
-							await MessageCounter().del_latest(UserID, Query['message']['message_id'])
-							return
-
-				if is_have_user_db:
-					if is_have_user_db[0][1] == 'advisor': # appstate
-						await Advisor().init(Query)
+				if 'text' in Query['message']:
+					if Query['message']['text'] == '/start':
+						from menu.menu import Menu
+						await Menu().init(Query['message']['from']['id'])
+						return
+					if Query['message']['text'] == '/clear':
+						await MessageCounter().del_latest(UserID, Query['message']['message_id'])
 						return
 
+				if UserData.is_advisee():
+					await Advisor().init(Query)
+					return
+
 		if 'callback_query' in Query:
-			if Query['callback_query']['data'] == 'Каталог':
-				await Catalog().init(Query)
-				return
-			if 'editarticle' in Query['callback_query']['data']:
-				await Catalog().init(Query)
-				return
-			if Query['callback_query']['data'] == 'order':
-				await Advisor().init(Query)
-				return
+			if 'private' in Query['callback_query']['message']['chat']['type']:
+
+				###################################################################
+				##################### private callback state ######################
+
+				if Query['callback_query']['data'] == 'Каталог':
+					await Catalog().init(Query)
+					return
+				if 'editarticle' in Query['callback_query']['data']:
+					await Catalog().init(Query)
+					return
+				if Query['callback_query']['data'] == 'order':
+					await Advisor().init(Query)
+					return
+				if Query['callback_query']['data'] == 'closeadvise':
+					await Advisor().init(Query)
+					return
 
 	###############################################################################
 
